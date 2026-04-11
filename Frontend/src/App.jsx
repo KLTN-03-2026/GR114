c5854bad1c714cc150f7dc35d0c67feee9258160
PhamHuuTuan0123 <phamhuutuandeptrai@gmail.com>
2026-04-11 15:30:04 +0700

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,3 +9,27 @@ export default function App() {
    );
 }
 
+// tuan //
+const CourseItem = (props) => {
+  return (
+  <div className="box">
+    <span className="amount">$59.99</span>
+    <img src="images/course-1.svg" alt="" />
+    <div className="stars">
+      <i className="fas fa-star" />
+      <i className="fas fa-star" />
+      <i className="fas fa-star" />
+      <i className="fas fa-star" />
+      <i className="far fa-star" />
+    </div>
+    <h3>{props.namecouse}</h3>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque ullam omnis odit accusamus doloremque? Cum aliquid unde nulla. Vitae, adipisci?</p>
+    <a href="#" className="btn">learn more</a>
+    <div className="icons">
+      <p> <i className="far fa-clock" /> 2 hours </p>
+      <p> <i className="far fa-calendar" /> 6 months </p>
+      <p> <i className="fas fa-book" /> 12 modules </p>
+    </div>
+  </div>
+  )
+}
